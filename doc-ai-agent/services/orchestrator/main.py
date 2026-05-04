0d42738227a3490032613f938b5319bb48ab9f26
Dharshan S Kotian <dcodecraft@gmail.com>
2026-05-04 17:30:25 +0530

    feat: add input validation and duplicate check nodes to main_grap, refactored files

--- doc-ai-agent/services/orchestrator/main.py
+++ doc-ai-agent/services/orchestrator/main.py
@@ -1,15 +1,29 @@
 import logging
-from config.settings import get_settings, configure_logging
+from config import get_settings, configure_logging
+import asyncio
 
+from doc_types.documents import NormalisedDocument
+from graph.workflow import run_workflow
 
-
-def main():
+async def main():
     settings = get_settings(service_name="orchestrator")
     configure_logging(settings)
 
     logger = logging.getLogger(__name__)
     logger.info("Orchestrator service starting")
 
+    #Example document for testing
+    doc = NormalisedDocument(
+        doc_id="123",
+        source="github",
+        title="Example Document",
+        content="This is an example document for testing.",
+        metadata={"author": "John Doe", "created_at": "2024-01-01"},
+    )
+
+    final_state = await run_workflow(doc)
+    logger.info("Orchestrator service finished")
+    logger.info(f"Final state: {final_state}")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
